95c974a7a122cb997831b9137d95f6bb83df02b2
Cruz Gerardo Yañez Teran <cruzgerardoyanezteran@Cruzs-MacBook-Pro.local>
2016-07-25 12:28:48 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Reparando Calles
+
+An IOs mobile app that take advantage of social networks in order to help local government to repair damage in urban works and broadcasting its results.
+
+[My personal webpage]
+
+### Version
+0.1
+
+
+License
+----
+
+Apache License
+
+
+**Free Software**
+
+[//]: # (Reference links)
+
+   [My personal webpage]: <http://geracoder.com>
---
+++ ReparandoCalles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReparandoCalles
+//
+//  Created by Cruz Gerardo Yañez Teran on 25/7/16.
+//  Copyright © 2016 Cruz Gerardo Yañez Teran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
